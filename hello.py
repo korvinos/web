5e7c39de74a2a352ec9a8a982eebabf2863e0cfd
korvinos <korvinos@bk.ru>
2016-03-05 10:22:11 +0300

    start django project

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 def app(environ, start_responce):
     status = '200 OK'
-    headers = [
-        ('Content-type', 'text/plan')
+    response_headers = [
+        ('Content-type', 'text/plain')
     ]
     data = environ['QUERY_STRING'].split('&')
     body = list(map(lambda x: x + '\n', data))
-    start_responce(status, headers)
+    start_responce(status, response_headers)
     return body
