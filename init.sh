20aac7bf64b445787189d6fdec24b2b49b2d6f81
korvinos <korvinos@bk.ru>
2016-03-05 11:15:48 +0300

    Validate url

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ sudo rm /etc/nginx/sites-avilable/default
 sudo ln -sf /home/korvinos/Documents/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/korvinos/Documents/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+sudo ln -sf /home/korvinos/Documents/web/etc/gunicorn_django.conf  /etc/gunicorn.d/test_django
 sudo /etc/init.d/gunicorn restart
