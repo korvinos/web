b54d52e17edd04f33c8fbdec9f900b3989ee72ad
korvinos <korvinos@bk.ru>
2016-03-05 11:02:55 +0300

    Validate url

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', test),
     url(r'^login/$', test),
     url(r'^signup/$', test),
-    url(r'^question\/\d+/$', test),
+    url(r'^question/([0-9]+)/$', test),
     url(r'^ask/$', test),
     url(r'^popular/$', test),
     url(r'^new/$', test),
